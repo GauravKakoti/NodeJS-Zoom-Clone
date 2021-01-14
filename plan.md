675a8479d7aaeb31f18daa9223638d32b0f50a66
Gaurav Kakoti <71715253+GauravKakoti@users.noreply.github.com>
2021-01-14 19:08:04 +0530

    Add files via upload

---
+++ plan.md
@@ -0,0 +1,11 @@
+# Plan of Action
+
+- Initialize our NodeJs project DONE
+- Initialize our first View DONE
+- Create a Room id DONE
+- Add the ability to view our own video DONE
+- Add the ability to allow others to stream their videos
+- Add Styling
+- Add the ability to Create messages
+- Add mute button
+- Add stop video button.
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+const { v4: uuidV4 } = require('uuid');
+
+app.use('/peerjs', peerServer);
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+/**
+ * @description Emits message to all users in a specific room
+ * @param room {string} - Room name/id
+ * @param emit {string} - Name of message to emit
+ * @param message {any} - Message to be sent
+ **/
+
+const sendToAllRoom = (room, emit, message) => {
+    for (let i in USER_LIST) {
+        if (USER_LIST[i].room == room) {
+            USER_LIST[i].socket.emit(emit, message);
+        }
+    }
+}
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
+      io.to(roomId).emit('createMessage', message)
+  }); 
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(process.env.PORT||3030)
